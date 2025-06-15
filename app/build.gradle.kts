b73450f4f7031118a1a0bacc4017516d8ebb2e8a
Anjana <anjanakaluarachchi2003@gmail.com>
2025-06-15 13:49:12 +0530

    API key exposed fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        manifestPlaceholders["google_maps_key"] = project.findProperty("google_maps_key") ?: ""
     }
 
     buildTypes {
